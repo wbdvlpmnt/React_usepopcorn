9927b31cb53cf39e5987fa1229200666c95816d7
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-12-11 10:11:45 -0600

    reusable star component

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      color={"red"}
+      className="test"
+      defaultrating={3}
+    />
   </React.StrictMode>
 );
