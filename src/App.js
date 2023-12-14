25800ca03783f7ce10d1f6080e8fd6ab5104fb2f
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-12-14 15:20:50 -0600

    add loader

--- src/App.js
+++ src/App.js
@@ -53,15 +53,18 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const query = "interstellar";
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const res = await fetch(`${process.env.REACT_APP_OMDB_URL}&s=${query}`);
       const data = await res.json();
       setMovies(data.Search);
       console.log(data.Search);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -74,9 +77,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
@@ -84,6 +85,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function Search() {
